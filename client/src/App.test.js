52fbbf5fc41235f78dcf10bf29c04c693ff4c816
jaumealg <38523661+jaumealg@users.noreply.github.com>
2020-09-29 19:51:04 +0200

    added dependencies

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,11 +1,6 @@
-//import React from 'react';
-//import ReactDOM from 'react-dom';
-//import App from './App';
-
-//it('renders without crashing', () => {});
-
 import React from 'react';
-import { render } from '@testing-library/react';
+import ReactDOM from 'react-dom';
 import App from './App';
 
-test('renders learn react link', () => {});
+it('renders without crashing', () => {});
+
